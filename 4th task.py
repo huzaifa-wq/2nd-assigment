81c65a6fd8037e3b74dc2a6c8f5bd516dc73ad22
huzaifa-wq <bhaihuzaifa733@gmail.com>
2026-01-16 00:52:31 +0500

    4th task

---
+++ 4th task.py
@@ -0,0 +1,11 @@
+first_num = float(input("Enter first number: "))
+second_num = float(input("Enter second number: "))
+therd_num = float(input("Enter third number: "))
+
+sum = first_num + second_num + therd_num
+product = first_num * second_num * therd_num
+average = sum / 3
+
+print("The sum is: ", sum)
+print("The product is: ", product)
+print("The average is: ", average)
